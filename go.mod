4dd470501a823859a19c9da0c88df7d75fd46442
TarasHrynchuk <125279218+TarasHrynchuk@users.noreply.github.com>
2024-03-01 16:45:38 +0100

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/my-org-org/self-consumer
+
+go 1.22
+
+require github.com/my-org-org/self-producer v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/my-org-org/self-producer/dependency"
+)
+
+func main() {
+	fmt.Println("Dependency Demo:")
+	dependency.PrintDependencyMessage()
+}
